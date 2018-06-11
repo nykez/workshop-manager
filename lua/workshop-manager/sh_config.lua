755f787059180cbf3d9d094d9112dc024f72852c
nykeztv <nykeztv@users.noreply.github.com>
2018-06-11 13:48:05 -0400

    Update sh_config.lua

--- lua/workshop-manager/sh_config.lua
+++ lua/workshop-manager/sh_config.lua
@@ -7,14 +7,6 @@ SimpleWorkshop.collectionID = "936489973"
 // Print to chat the addons they're missing //
 SimpleWorkshop.print = false
 
-local function client(file)
-	if SERVER then AddCSLuaFile(file) end
-
-	if CLIENT then
-		return include(file)
-	end
-end
-
 if SERVER then
     AddCSLuaFile("cl_init.lua")
 end
